b355ed38c766e33440d76a38c394456a07efc12c
Denys Shapovalov <denys.shapovalov@outlook.com>
2025-03-31 21:16:47 +0200

    Update Cypher.cs
    Deleting namespace for unnecessary use

--- PairedLetterSubstitutionCipher/PairedLetterSubstitutionCipher/Cypher.cs
+++ PairedLetterSubstitutionCipher/PairedLetterSubstitutionCipher/Cypher.cs
@@ -1,38 +1,37 @@
-﻿namespace GaderypolukiCipher
-{
-    class Cypher() {
-        public static string GaderypolukiDictionary(string input)
+
+class Cypher() {
+    public static string GaderypolukiDictionary(string input)
+    {
+        var dict = new Dictionary<char, char>
         {
-            var dict = new Dictionary<char, char>
-            {
-                {'G', 'A'},
-                {'A', 'G'},
-                {'D', 'E'},
-                {'E', 'D'},
-                {'R', 'Y'},
-                {'Y', 'R'},
-                {'P', 'O'},
-                {'O', 'P'},
-                {'L', 'U'},
-                {'U', 'L'},
-                {'K', 'I'},
-                {'I', 'K'}
-            };
+            {'G', 'A'},
+            {'A', 'G'},
+            {'D', 'E'},
+            {'E', 'D'},
+            {'R', 'Y'},
+            {'Y', 'R'},
+            {'P', 'O'},
+            {'O', 'P'},
+            {'L', 'U'},
+            {'U', 'L'},
+            {'K', 'I'},
+            {'I', 'K'}
+        };
 
-            input = input.ToUpper();
-            string output = string.Empty;
+        input = input.ToUpper();
+        string output = string.Empty;
 
-            foreach (char c in input)
-            {
-                if (dict.ContainsKey(c)) {
-                    output += dict[c];
-                }
-                else {
-                    output += c;
-                }
+        foreach (char c in input)
+        {
+            if (dict.ContainsKey(c)) {
+                output += dict[c];
             }
-
-            return output;
+            else {
+                output += c;
+            }
         }
+
+        return output;
     }
 }
+
