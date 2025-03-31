b7a820623ddbf2d215dad083c435eb6206a28161
Denys Shapovalov <denys.shapovalov@outlook.com>
2025-03-31 17:23:58 +0200

    Creating an encypher using Gaderypoluki Cypher

---
+++ PairedLetterSubstitutionCipher/PairedLetterSubstitutionCipher/Cypher.cs
@@ -0,0 +1,38 @@
+﻿namespace GaderypolukiCipher
+{
+    class Cypher() {
+        public static string GaderypolukiDictionary(string input)
+        {
+            var dict = new Dictionary<char, char>
+            {
+                {'G', 'A'},
+                {'A', 'G'},
+                {'D', 'E'},
+                {'E', 'D'},
+                {'R', 'Y'},
+                {'Y', 'R'},
+                {'P', 'O'},
+                {'O', 'P'},
+                {'L', 'U'},
+                {'U', 'L'},
+                {'K', 'I'},
+                {'I', 'K'}
+            };
+
+            input = input.ToUpper();
+            string output = string.Empty;
+
+            foreach (char c in input)
+            {
+                if (dict.ContainsKey(c)) {
+                    output += dict[c];
+                }
+                else {
+                    output += c;
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ PairedLetterSubstitutionCipher/PairedLetterSubstitutionCipher/Program.cs
@@ -0,0 +1,9 @@
+﻿using GaderypolukiCipher;
+
+Console.WriteLine("Write text to encript");
+
+string input = Console.ReadLine();
+
+string output = Cypher.GaderypolukiDictionary(input);
+
+Console.WriteLine("Encrupted text is:  " + output);
